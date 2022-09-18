5ed8d50b5e55a4e67bab98403438e2a350025e65
root <joshuamutiso455@gmail.com>
2022-09-18 14:25:31 +0300

    ssh

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,19 @@
-# Create the ~/.ssh/config with puppet
-file {'~/.ssh/config':
-  ensure  => present,
-  replace => 'yes',
-  path    => '/etc/ssh/ssh_config',
-  content => 'Host *
-     HostName 3.235.148.57
-     User root
-     IdentityFile ~/.ssh/school',
-  mode    => '0664',
+#Set up ssh_config to reject password authentication
+
+include stdlib
+
+file_line { 'SSH Private Key':
+  path               => '/etc/ssh/ssh_config',
+  line               => '    IdentityFile ~/.ssh/school',
+  match              => '^[#]+[\s]*(?i)IdentityFile[\s]+~/.ssh/id_rsa$',
+  replace            => true,
+  append_on_no_match => true
+}
+
+file_line { 'Deny Password Auth':
+  path               => '/etc/ssh/ssh_config',
+  line               => '    PasswordAuthentication no',
+  match              => '^[#]+[\s]*(?i)PasswordAuthentication[\s]+(yes|no)$',
+  replace            => true,
+  append_on_no_match => true
 }
