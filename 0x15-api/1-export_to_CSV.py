efe73caf6666ad146ae0a29608578b09e7d3d2e4
joshuamutiso <joshuamutiso455@gmail.com>
2022-10-17 20:20:08 +0300

    api

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python3
 """Get info form Api and Save CSV"""
 import csv
-import sys
 import requests
+from sys import argv
 
 
 if __name__ == "__main__":
 
-    user_id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(user_id)).json()
-    username = user.get("username")
-    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+    users = requests.get(
+        "https://jsonplaceholder.typicode.com/users?id=" + argv[1])
+    todos = requests.get(
+        "https://jsonplaceholder.typicode.com/todos?userId=" + argv[1])
 
-    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
-        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        [writer.writerow(
-            [user_id, username, t.get("completed"), t.get("title")]
-        ) for t in todos]
+    users_json = users.json()
+    todos_json = todos.json()
+    task_list = []
+    employee_name = users_json[0]['username']
+
+    with open(str(argv[1])+'.csv', 'w') as f:
+        write = csv.writer(f, quoting=csv.QUOTE_ALL)
+        for data in todos_json:
+            write.writerow([str(argv[1]), str(employee_name),
+                            data['completed'], data['title']])
